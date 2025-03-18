d7a419bd62b1a5f219d6cde057f949b265043636
Brett <brettearle@gmail.com>
2025-03-19 06:30:04 +1030

    FEAT<init>moved wait into own package

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	ctx := context.Background()
-	if err := app.Run(ctx, app.NewConfig("127.0.0.1", "6969"), os.Stdout, os.Stderr); err != nil {
+	if err := app.Run(ctx, app.NewConfig("127.0.0.1", "8989"), os.Stdout, os.Stderr); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
--- main_test.go
+++ main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -10,65 +9,21 @@ import (
 	"time"
 
 	"github.com/brettearle/aesil/internal/app"
+	"github.com/brettearle/aesil/internal/wait"
 )
-
-// waitForReady calls the specified endpoint until it gets a 200
-// response or until the context is cancelled or the timeout is
-// reached.
-func waitForReady(
-	ctx context.Context,
-	timeout time.Duration,
-	endpoint string,
-) error {
-	client := http.Client{}
-	startTime := time.Now()
-	for {
-		req, err := http.NewRequestWithContext(
-			ctx,
-			http.MethodGet,
-			endpoint,
-			nil,
-		)
-		if err != nil {
-			return fmt.Errorf("failed to create request: %w", err)
-		}
-
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Printf("Error making request: %s\n", err.Error())
-			continue
-		}
-		if resp.StatusCode == http.StatusOK {
-			fmt.Println("Endpoint is ready!")
-			resp.Body.Close()
-			return nil
-		}
-		resp.Body.Close()
-
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		default:
-			if time.Since(startTime) >= timeout {
-				return fmt.Errorf("timeout reached while waiting for endpoint")
-			}
-			// wait a little while between checks
-			time.Sleep(250 * time.Millisecond)
-		}
-	}
-}
 
 func TestEndPoints(t *testing.T) {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	t.Cleanup(cancel)
-	go app.Run(ctx, app.NewConfig("127.0.0.1", "6969"), os.Stdout, os.Stderr)
-	waitForReady(ctx, time.Second, "http://127.0.0.1:6969/ping")
+	go app.Run(ctx, app.NewConfig("127.0.0.1", "8989"), os.Stdout, os.Stderr)
+	wait.ForServerReady(ctx, time.Second, "http://127.0.0.1:8989/ping")
 
 	t.Run("/ping", func(t *testing.T) {
+		t.Parallel()
 		want := "Pong"
 		client := http.Client{}
-		req, _ := http.NewRequest("GET", "http://127.0.0.1:6969/ping", nil)
+		req, _ := http.NewRequest("GET", "http://127.0.0.1:8989/ping", nil)
 		res, err := client.Do(req)
 		if err != nil {
 			t.Errorf("got error %v", err)
